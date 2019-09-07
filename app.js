cfb5293d35faebc4122420f7280581dc051bae34
sajalkhan <sajalhossen@yahoo.com>
2019-09-07 23:29:19 +0600

    Read and write file

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const fs = require('fs');  // include file module to read and write 
 
-const read_data = fs.readFileSync('./testFile.txt','utf-8');
+//non-blocking asynchronous way
+fs.readFile('./testFile.txt', 'utf-8', (err, data) => {
+    console.log(data);
+    fs.writeFile('./testFile2.txt',data,'utf-8',err =>{
+        console.log('file has been written');
+    });
+});
 
-console.log(read_data);
-
-//const textIn = 'output of this text is:  ' + read_data; // first way
-const textIn = `Output of this text is: ${read_data}\n Created on ${Date.now()}`; // second way and we can do any calculation in here {}
-console.log(textIn);
-
-fs.writeFileSync('./output.txt',textIn); // write something in a file
-console.log('file written!');
+console.log('read data!');
