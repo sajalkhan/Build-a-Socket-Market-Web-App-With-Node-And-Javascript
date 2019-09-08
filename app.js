7133c1ff27d01ac97c42d9b262fd69c3e12c6049
sajalkhan <sajalhossen@yahoo.com>
2019-09-08 22:52:27 +0600

    Express Basic Routing 1.0

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const fs = require('fs');  // include file module to read and write 
 const http = require('http'); // include http module
 const url = require('url'); // include url module to get url related information
+const express = require('express'); // include express in our app
 
+const app = express();
+const port = 8080;
+
+app.get('/', (req,res)=>{
+
+    // res.status(200).send('Welcome to Node.js Express!');
+    res.status(200).json({message:'Welcome to node.js express!', app:'new app'});
+});
+
+app.post('/', (req,res)=>{
+
+    res.send('yes it is a post request!');
+});
+
+app.listen(port, ()=>{
+    console.log(`App running on ${port}...`);
+});
 
 
