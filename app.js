859787887292f58dcc1e23100c348ea38d7d9cc6
sajalkhan <sajalhossen@yahoo.com>
2019-09-14 16:19:32 +0600

    install handlebars

--- app.js
+++ app.js
@@ -2,30 +2,24 @@ const fs = require('fs');  // include file module to read and write
 const http = require('http'); // include http module
 const url = require('url'); // include url module to get url related information
 const express = require('express'); // include express in our app
+const path = require('path'); //include path
+const exphbs  = require('express-handlebars');
 
 const app = express();
-const port = 8080;
+const port =  process.env.PORT || 8080;
 
-app.use(express.static('public')); // now we can use static file
+//set Handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-app.get('/index.html',(req,res)=>{
+app.use(express.static(path.join(__dirname,'public'))); // now we can use static file
 
-    res.sendfile(__dirname+"/index.html");
+//set handle bars routs
+app.get('/',()=>{
+
+    res.render('home');
 });
 
-app.get('/process_get', (req, res) => {
-    
-    response = {
-        first_name : req.query.first_name,
-        last_name : req.query.last_name
-    };
-
-    console.log(response);
-    res.end(JSON.stringify(response));
-});
-
-app.listen(port, () => {
-    console.log(`App running on ${port}...`);
-});
+app.listen(port, () => { console.log(`App running on ${port}...`); });
 
 
