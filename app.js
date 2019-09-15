165b0303f0a535125aa52c04ca4c68d8da69b117
sajalkhan <sajalhossen@yahoo.com>
2019-09-15 20:26:25 +0600

    create a callback function to get API data and show in our web page

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const url = require('url'); // include url module to get url related information
 const path = require('path'); //include path
 
 const express = require('express'); // include express in our app
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const request = require('request');
 
 const app = express();
-const port =  process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 //set Handlebars middleware
 app.engine('handlebars', exphbs());
@@ -15,22 +16,37 @@ app.set('view engine', 'handlebars');
 
 //API key: pk_a4db854aeb4c4f18bd467fd70841b876
 
+function call_api(finishApi) {
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_a4db854aeb4c4f18bd467fd70841b876', { json: true }, (err, res, body) => {
+
+        if (err) return console.log(err);
+         
+        if (res.statusCode == 200) {
+            finishApi(body);
+        };
+
+    });
+}
+
 //set handle bars routs
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
 
-    res.render('home',{
-        status:"now we can pass data :)"
+    call_api(function (doneApi) {  // it's a callback function
+        res.render('home', {
+            stock: doneApi
+        });
+        // console.log(doneApi);
     });
 });
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
 
-    res.render('about',{
-        status:"yes about page is working perfectly :)"
+    res.render('about', {
+        status: "yes about page is working perfectly :)"
     });
 });
 
-app.use(express.static(path.join(__dirname,'public'))); // now we can use static file
+app.use(express.static(path.join(__dirname, 'public'))); // now we can use static file
 app.listen(port, () => { console.log(`App running on ${port}...`); });
 
 
